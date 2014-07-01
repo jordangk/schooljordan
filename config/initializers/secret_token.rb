b0e68570ec5a5cc3e662ca3e33bf64e2ccb3d202
Jordan <jordan@wishpond.com>
2014-07-01 16:57:09 -0700

    adding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jordan::Application.config.secret_key_base = 'd617f069aa1e73693aa218b88ad23ecf4aee4ac35038690f6b13b95bf18b7898340334a68effbc213c94cafeeb6713d1f130710672d64daf87adba871d304077'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jordan::Application.config.session_store :cookie_store, key: '_jordan_session'
